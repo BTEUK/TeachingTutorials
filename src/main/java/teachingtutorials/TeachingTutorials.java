c84cedf464cf6555eff15ec531ed260468a5ea86
george112n <georgewalk168@gmail.com>
2022-02-12 15:44:21 +0000

    rdggdf

---
+++ src/main/java/teachingtutorials/TeachingTutorials.java
@@ -0,0 +1,17 @@
+package teachingtutorials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TeachingTutorials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

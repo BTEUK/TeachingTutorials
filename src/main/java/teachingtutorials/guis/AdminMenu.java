04133e79ed1a73b4c897bdcb47565256803324e4
george112n <georgewalk168@gmail.com>
2023-03-22 12:15:58 +0000

    Changed the icons and added a back button

--- src/main/java/teachingtutorials/guis/AdminMenu.java
+++ src/main/java/teachingtutorials/guis/AdminMenu.java
@@ -42,16 +42,18 @@ public class AdminMenu
         //Menu for admins
         if (u.player.hasPermission("TeachingTutorials.Admin"))
         {
-            Utils.createItem(inventory, Material.BOOKSHELF, 1, 12,(ChatColor.GREEN +"Set Compulsory Tutorial"), ChatColor.DARK_GREEN+"Admins only");
+            Utils.createItem(inventory, Material.IRON_BARS, 1, 12,(ChatColor.GREEN +"Set Compulsory Tutorial"), ChatColor.DARK_GREEN+"Admins only");
             Utils.createItem(inventory, Material.BOOKSHELF, 1, 14,(ChatColor.GREEN +"My Tutorials"), ChatColor.DARK_GREEN+"View the tutorials you have created", ChatColor.DARK_GREEN+"Add locations", ChatColor.DARK_GREEN+"Set tutorials as in use");
-            Utils.createItem(inventory, Material.BOOKSHELF, 1, 16,(ChatColor.GREEN +"Create Tutorial"), ChatColor.DARK_GREEN+"Create a new tutorial in game");
+            Utils.createItem(inventory, Material.KNOWLEDGE_BOOK, 1, 16,(ChatColor.GREEN +"Create Tutorial"), ChatColor.DARK_GREEN+"Create a new tutorial in game");
         }
         //Menu for creators only
         else
         {
             Utils.createItem(inventory, Material.BOOKSHELF, 1, 12,(ChatColor.GREEN +"My Tutorials"), ChatColor.DARK_GREEN+"View the tutorials you have created", ChatColor.DARK_GREEN+"Add locations", ChatColor.DARK_GREEN+"Set tutorials as in use");
-            Utils.createItem(inventory, Material.BOOKSHELF, 1, 16,(ChatColor.GREEN +"Create Tutorial"), ChatColor.DARK_GREEN+"Create a new tutorial in game");
+            Utils.createItem(inventory, Material.KNOWLEDGE_BOOK, 1, 16,(ChatColor.GREEN +"Create Tutorial"), ChatColor.DARK_GREEN+"Create a new tutorial in game");
         }
+
+        Utils.createItem(inventory, Material.SPRUCE_DOOR, 1, 27, ChatColor.BOLD +"" +ChatColor.GREEN+"Back to main menu");
 
         toReturn.setContents(inventory.getContents());
 
@@ -67,37 +69,44 @@ public class AdminMenu
         {
             player.closeInventory();
             player.openInventory(CompulsoryTutorialMenu.getGUI());
+            return;
+        }
+
+        //Finds the correct user for this player from the plugins list of users
+        boolean bUserFound = false;
+
+        ArrayList<User> users = plugin.players;
+        int iLength = users.size();
+        int i;
+        User user = new User(player);
+
+        for (i = 0 ; i < iLength ; i++)
+        {
+            if (users.get(i).player.getUniqueId().equals(player.getUniqueId()))
+            {
+                user = users.get(i);
+                bUserFound = true;
+                break;
+            }
+        }
+
+        if (!bUserFound)
+        {
+            player.sendMessage(ChatColor.RED +"An error occurred. Please contact a support staff. Error: 1");
+            player.sendMessage(ChatColor.RED +"Try relogging");
+            return;
         }
 
         else if (clicked.getItemMeta().getDisplayName().equals(ChatColor.GREEN +"My Tutorials"))
         {
-            //Finds the correct user for this player from the plugins list of users
-            boolean bUserFound = false;
-
-            ArrayList<User> users = plugin.players;
-            int iLength = users.size();
-            int i;
-            User user = new User(player);
-
-            for (i = 0 ; i < iLength ; i++)
-            {
-                if (users.get(i).player.getUniqueId().equals(player.getUniqueId()))
-                {
-                    user = users.get(i);
-                    bUserFound = true;
-                    break;
-                }
-            }
-
-            if (!bUserFound)
-            {
-                player.sendMessage(ChatColor.RED +"An error occurred. Please contact a support staff. Error: 1");
-                player.sendMessage(ChatColor.RED +"Try relogging");
-                return;
-            }
-
             player.closeInventory();
             player.openInventory(CreatorTutorialsMenu.getGUI(user));
         }
+        else if (slot+1 == 27)
+        {
+            player.closeInventory();
+            player.openInventory(MainMenu.getGUI(user));
+        }
+
     }
 }

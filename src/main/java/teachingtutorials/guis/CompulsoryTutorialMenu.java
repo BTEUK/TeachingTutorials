a992063188b3894210b3f71d5b7efbb4f10c8a75
george112n <georgewalk168@gmail.com>
2023-03-22 12:13:48 +0000

    Added a back button

--- src/main/java/teachingtutorials/guis/CompulsoryTutorialMenu.java
+++ src/main/java/teachingtutorials/guis/CompulsoryTutorialMenu.java
@@ -12,13 +12,12 @@ import teachingtutorials.utils.User;
 import teachingtutorials.utils.Utils;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class CompulsoryTutorialMenu
 {
     public static Inventory inventory;
     public static String inventory_name;
-    public static int inv_rows = 3 * 9;
+    public static int iRows;
     public static TeachingTutorials plugin;
 
     private static Tutorial[] tutorials;
@@ -28,9 +27,7 @@ public class CompulsoryTutorialMenu
 
     public static void initialize()
     {
-        inventory_name = ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Compulsory tutorial selection";
-
-        inventory = Bukkit.createInventory(null, inv_rows);
+        inventory_name = ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Select Compulsory Tutorial";
     }
 
     public static String getInventoryName()
@@ -40,14 +37,42 @@ public class CompulsoryTutorialMenu
 
     public static Inventory getGUI ()
     {
+        //Declare variables
         int i;
-
-        Inventory toReturn = Bukkit.createInventory(null, inv_rows, inventory_name);
-
-        inventory.clear();
+        int iTutorials;
+        int iDiv;
+        int iMod;
 
         //Admins can only select from tutorials which have been set by creators as in use
         tutorials = Tutorial.fetchAll(true);
+
+        //Works out how many rows in the inventory are needed
+        iTutorials = tutorials.length;
+        iDiv = iTutorials/9;
+        iMod = iTutorials%9;
+
+        if (iMod != 0 || iDiv == 0)
+        {
+            iDiv = iDiv + 1;
+        }
+
+        //Enables an empty row and then a row for the back button
+        iRows = iDiv+2;
+
+        //Create inventories
+        inventory = Bukkit.createInventory(null, iRows * 9);
+        inventory.clear();
+
+        Inventory toReturn = Bukkit.createInventory(null, iRows * 9, inventory_name);
+
+        //Indicates that the creator has no tutorials if they don't own any
+        if (iTutorials == 0)
+        {
+            Utils.createItem(inventory, Material.BARRIER, 1, 5, ChatColor.BOLD +"" +ChatColor.GREEN +"There are no in-use tutorials on the system");
+        }
+
+        //Adds back button
+        Utils.createItem(inventory, Material.SPRUCE_DOOR, 1, iRows * 9, ChatColor.BOLD +"" +ChatColor.GREEN+"Back to creator menu");
 
         //Inv slot 1 = the first one
         //Add the tutorials to the gui
@@ -72,12 +97,45 @@ public class CompulsoryTutorialMenu
         //When player clicks on one of the tutorials
         if (clicked.getType().equals(compulsoryBlock) || clicked.getType().equals(nonCompulsoryBlock))
         {
-            //Sets the current compulsory tutorial to not compulsory - ID is used to identify the tutorial
+            //Toggles whether this tutorial is compulsory or not - ID is used to identify the tutorial
             tutorials[slot].toggleCompulsory();
 
             //Refreshes the display
             player.closeInventory();
             player.openInventory(CompulsoryTutorialMenu.getGUI());
+            return;
+        }
+
+        if (slot+1 == iRows*9)
+        {
+            //Finds the correct user for this player from the plugins list of users
+            boolean bUserFound = false;
+
+            ArrayList<User> users = plugin.players;
+            int iLength = users.size();
+            int i;
+            User user = new User(player);
+
+            for (i = 0 ; i < iLength ; i++)
+            {
+                if (users.get(i).player.getUniqueId().equals(player.getUniqueId()))
+                {
+                    user = users.get(i);
+                    bUserFound = true;
+                    break;
+                }
+            }
+
+            if (!bUserFound)
+            {
+                player.sendMessage(ChatColor.RED +"An error occurred. Please contact a support staff. Error: 1");
+                player.sendMessage(ChatColor.RED +"Try relogging");
+                return;
+            }
+
+            //Back button
+            player.closeInventory();
+            player.openInventory(AdminMenu.getGUI(user));
         }
     }
 }

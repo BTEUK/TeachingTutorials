f72dae1a3bcb712802ed64cfc927b1939455a8ae
george112n <georgewalk168@gmail.com>
2023-06-13 12:21:58 +0100

    Changes the event priority to have the Tutorials listener deal with a tpll first. Adds a deregister method for use when a tutorial or location creation is terminated early.

--- src/main/java/teachingtutorials/newlocation/StartLocationListener.java
+++ src/main/java/teachingtutorials/newlocation/StartLocationListener.java
@@ -7,6 +7,7 @@ import net.buildtheearth.terraminusminus.util.geo.LatLng;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -40,7 +41,8 @@ public class StartLocationListener implements Listener
         this.projection = projection;
     }
 
-    @EventHandler
+    //Want the tutorials tpll process to occur first
+    @EventHandler(priority = EventPriority.LOWEST)
     public void tpllCommand(PlayerCommandPreprocessEvent event)
     {
         if (event.getPlayer().getUniqueId().equals(this.creator.player.getUniqueId()))
@@ -92,4 +94,8 @@ public class StartLocationListener implements Listener
         Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
+    public void deregister()
+    {
+        HandlerList.unregisterAll(this);
+    }
 }

eac5dcf234368e6304853da9c2511201e8fa3ed0
George <georgewalk168@gmail.com>
2024-08-25 11:01:06 +0100

    Adds a secondary list storing what the original block that each virtual block was, it will add to that list when adding a new virtual block to the main list.

--- src/main/java/teachingtutorials/utils/VirtualBlockGroup.java
+++ src/main/java/teachingtutorials/utils/VirtualBlockGroup.java
@@ -6,10 +6,10 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
+import teachingtutorials.TeachingTutorials;
 import teachingtutorials.TutorialPlaythrough;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -21,6 +21,10 @@ import java.util.concurrent.ConcurrentHashMap;
 public class VirtualBlockGroup<K, V> extends ConcurrentHashMap<K,V>
 {
     private final TutorialPlaythrough tutorialPlaythrough;
+
+    //A list of real world blocks. This list runs parallel to the virtual blocks group list
+    //WARNING: This may not hold the true value of the block. It is important to reset all virtual blocks groups in the reverse order to which they were created
+    private ConcurrentHashMap<Location, BlockData> realWorldBlocks = new ConcurrentHashMap<>();
 
     //A list of spies also viewing the virtual blocks
     private ArrayList<Player> spies = new ArrayList<>();
@@ -38,6 +42,24 @@ public class VirtualBlockGroup<K, V> extends ConcurrentHashMap<K,V>
     public boolean isOfPlaythrough(TutorialPlaythrough tutorialPlaythrough)
     {
         return tutorialPlaythrough.equals(this.tutorialPlaythrough);
+    }
+
+    @Override
+    public V put(K key, V value)
+    {
+        //Find the real value for this block and record this
+        if (key instanceof Location)
+        {
+            //Store a reference to the world locally
+            World worldToRead = tutorialPlaythrough.getLocation().getWorld();
+
+            BlockData realBlock = worldToRead.getBlockData((Location) key).clone(); //This actually gets run asynchronously and takes over 2 seconds sometimes
+
+            Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_BLUE +"Adding block to list and block's original material/: "+realBlock.getMaterial());
+
+            realWorldBlocks.put((Location) key, realBlock);
+        }
+        return super.put(key, value);
     }
 
     /**
@@ -96,40 +118,43 @@ public class VirtualBlockGroup<K, V> extends ConcurrentHashMap<K,V>
         //Get the blocks in the world
         for (int i = 0 ; i < iLocations ; i++)
         {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA +"Setting a virtual block to the actual world");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA +"Setting a " +virtualBlockData[i].getMaterial() +" block to the actual world");
             worldToSet.setBlockData(locations[i], virtualBlockData[i]);
         }
     }
 
     /**
-     * Creates a map of location to real blocks at the locations of the virtual blocks of this list
+     * Resets the world back to its original state.
+     * <P> </P>
+     * When using this method with lots of virtual block groups it is important to call the resets from later virtual block groups first. As in FILO.
+     */
+    public void resetWorld()
+    {
+        //Store a reference to the world locally
+        World worldToSet = tutorialPlaythrough.getLocation().getWorld();
+
+        //Gets the list of locations and block data for this list
+        final Location[] locations = realWorldBlocks.keySet().stream().toArray(Location[]::new);
+        final BlockData[] virtualBlockData = realWorldBlocks.values().toArray(BlockData[]::new);
+        int iLocations = locations.length;
+
+//        Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA +"Resetting the blocks to the world");
+
+        Bukkit.getScheduler().runTask(TeachingTutorials.getInstance(), () -> {
+            //Set the blocks in the world
+            for (int i = 0 ; i < iLocations ; i++)
+            {
+                worldToSet.setBlockData(locations[i], virtualBlockData[i]);
+            }
+        });
+    }
+
+    /**
+     * Returns the map of location to real blocks at the locations of the virtual blocks of this list
      * @return A list of the real world blocks at locations of the virtual blocks of this list
      */
     public Map<Location, BlockData> getRealBlocks()
     {
-        //Store a reference to the world locally
-        World worldToRead = tutorialPlaythrough.getLocation().getWorld();
-
-        //Gets the list of locations for this list
-        Location[] virtualBlockLocations = this.keySet().stream().toArray(Location[]::new);
-        int iLocations = virtualBlockLocations.length;
-
-        //Creates a map holding the world's real data at the locations of the virtual blocks
-        Map<Location, BlockData> realWorldBlocks = new HashMap<>(iLocations);
-        for (int i = 0 ; i < iLocations ; i++)
-        {
-            BlockData realBlock = worldToRead.getBlockData(virtualBlockLocations[i]).clone(); //This actually gets run asynchronously and takes over a second sometimes
-
-//            Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_BLUE +"Block recorded at ("
-//                    +virtualBlockLocations[i].getX()+","
-//                    +virtualBlockLocations[i].getY()+","
-//                    +virtualBlockLocations[i].getZ()
-//                    +") with material: "+realBlock.getMaterial());
-
-            realWorldBlocks.put(virtualBlockLocations[i], realBlock);
-
-        }
-
         return realWorldBlocks;
     }
 
